915754edb7eec1c14162135be925d0e922d3ee35
Bipin Kumar <bipinkumar@Bipins-MacBook-Pro.local>
2024-10-16 10:46:18 +0530

    Initial commit

---
+++ src/pages/UpcomingEvents.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const UpcomingEvents = () => {
+  return (
+    <div className='max-w-[1200px] mx-auto'>
+        <div className="title">
+            <h2 className='text-4xl'>Upcoming Events</h2>
+        </div>
+        <div className='card-box'>
+            <div className="thumbnail">
+                <img src="" alt="" />
+                <div className="bookmark-icon">
+                    
+                </div>
+            </div>
+            <div className="card-content">
+                <h4>Globally Startup Summit 2024 | Delhi NCR</h4>
+                <div>
+                    July, 13, 2024
+                </div>
+                <div>
+                    <div>
+                    {/**Icon */}
+                    </div>
+                    <div>
+                        Professional
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UpcomingEvents
