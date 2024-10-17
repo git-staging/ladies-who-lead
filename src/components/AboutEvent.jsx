c4c45b1374bdf23aef02c9dee8b26a913c3ac78d
Bipin Kumar <bipinkumar@Bipins-MacBook-Pro.local>
2024-10-17 23:06:16 +0530

    deploying..

--- src/components/AboutEvent.jsx
+++ src/components/AboutEvent.jsx
@@ -5,6 +5,7 @@ const AboutEvent = () => {
   return (
     <div className='container mx-auto'>
         <div className='flex flex-col gap-4'>
+            <h2 className='text-4xl py-4'>About The Event</h2>
             <h3>Global Startup Summit 2024 | Delhi NCR</h3>
             <h4>Past Agenda,</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
